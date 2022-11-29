d382bd825ebf2e0042d49d9f3a391475426df35b
Albin Forsberg <foal21fs@student.ju.se>
2022-11-29 14:10:32 +0100

    Added lab 8

--- lab7/main.py
+++ lab7/main.py
@@ -1,37 +1,37 @@
 import socket
 import select
 
-PORT = 8080
+PORT = 60003
 sockL = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sockL.bind(('', PORT))
+sockL.bind(("", PORT))
 sockL.listen(1)
 
 listOfSockets = [sockL]
 
 print("Listening on port {}".format(PORT))
 
+
 while True:
     tup = select.select(listOfSockets, [], [])
     sock = tup[0][0]
 
     if sock == sockL:
-        (sockC, addr) = sockL.accept()
-        listOfSockets.append(sockC)
+        (sockClient, addr) = sockL.accept()
+        sock = sockClient
+        listOfSockets.append(sock)
         for s in listOfSockets:
             if s != sockL:
                 s.send("[{}{}] (connected)".format(addr[0], addr[1]).encode())
-
     else:
         data = sock.recv(1024)
         if not data:
+            print("Client disconnected")
             listOfSockets.remove(sock)
+            sock.close()
             for s in listOfSockets:
                 if s != sockL:
                     s.send("[{}{}] (disconnected)".format(addr[0], addr[1]).encode())
-            sock.close()
-            continue
         else:
             for s in listOfSockets:
                 if s != sockL:
                     s.send("[{}{}] {}".format(addr[0], addr[1], data.decode()).encode())
-
