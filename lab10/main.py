d871aee14b1969b8f7150e97f852be17664ce9c6
Albin Forsberg <foal21fs@student.ju.se>
2022-11-29 22:53:53 +0100

    Started working on lab10

---
+++ lab10/main.py
@@ -0,0 +1,30 @@
+import re
+
+def main():
+    print("Hello World!")
+
+    mtxt = "jox r.nohre@jth.hj.se, bjox@se, adam@example.com, jox@jox@jox.com"
+
+    print(find_email1(mtxt))
+
+
+
+def find_email1(emails):
+    email_regex = r"[\s]+[\w\.]*?@[\w]+\.[\w]+"
+
+    return re.findall(email_regex, emails)
+
+def open_file(name):
+    try:
+        with open(name, "r") as f:
+            return f.read()
+    except FileNotFoundError:
+        print("File not found")
+        return None
+
+def simpsons():
+    file = op
+
+
+if __name__ == "__main__":
+    main()
